ec2dedc8858930de6304eb7ae4246effc6473689
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2023-06-09 01:00:18 +0300

    Add catch error state

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 function Quote() {
   const [quote, setQuote] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  // const [hasError, setHasError] = useState(false)
+  const [hasError, setHasError] = useState(false);
 
   const apiKey = 'rruW2yeTLFMLFy5iDM0R8w==YVB0xOk1qJHHR1fZ';
   const api = 'https://api.api-ninjas.com/v1/quotes';
@@ -12,32 +12,25 @@ function Quote() {
     const fetchQuote = async () => {
       try {
         setIsLoading(true);
-        const res = await fetch(api,
-          {
-            method: 'GET',
-            withCredentials: true,
-            headers: {
-              'X-Api-Key': `${apiKey}`,
-              'Content-Type': 'application/json',
-            },
-          });
+        const res = await fetch(api, { headers: { 'X-Api-Key': `${apiKey}` } });
         const quoteData = await res.json();
         setQuote(quoteData);
-        setIsLoading(false);
       } catch (err) {
-        console.log(err);
-        // setHasError(true)
+        setHasError(true);
       }
+      setIsLoading(false);
     };
     fetchQuote();
-  }, [setQuote]);
+  }, [setQuote, setIsLoading]);
+
+  if (hasError) { <div>Something went wrong...</div>; }
 
   if (isLoading) {
     return <div>Loading...</div>;
   }
 
   return (
-    <div>{quote.map((item) => item.quote)}</div>
+    <div>{quote.length > 0 ? quote.map((item) => item.quote) : 'Something went wrong'}</div>
   );
 }
 
